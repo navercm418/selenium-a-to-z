efd8fe4fe59cedada7eed46c1a3e848a712d7fdf
Andy M <navercm519@gmail.com>
2021-08-10 07:48:09 -0400

    bedfor search

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from myutils import zvFileUtil
-import sqlite3
-import sys
-import os
+from subprocess import Popen, PIPE, STDOUT
 
 
+zvFileName = 'names_list2.txt'
+zvTown = 'bedford'
+zvName = ''
 
+zvNameLst = zvFileUtil(zvFileName, 'r')
+zvNameLst = zvNameLst.splitlines()
+
+for n in zvNameLst:
+
+    zvName = n
+
+    # python3 AtoZ.py tabayoyong randolph Massachusetts --headless log-level=3
+    cmd = r'C:\Users\amcc4\AppData\Local\Programs\Python\Python39\python.exe AtoZ.py '+ zvName +' '+ zvTown +' Massachusetts --headless log-level=3'
+    print(cmd)
+    prc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    prc.wait()
