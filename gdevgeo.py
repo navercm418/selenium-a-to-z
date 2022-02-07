0e2d319aa8daa169960fe922057f917b93088404
Andy M <navercm519@gmail.com>
2022-02-07 11:30:18 -0500

    geodev

--- gdevgeo.py
+++ gdevgeo.py
@@ -42,10 +42,14 @@ zvAdrLst = zvAdrLst.splitlines()
 for a in zvAdrLst:    
 
     zvAdId = a.split('|')[0]
-    zvAdr1 = a.split('|')[1]
-    zvZip1 = a.split('|')[2]
-    zvLat1 = a.split('|')[3]
-    zvLon1 = a.split('|')[4]
+    zvSta1 = a.split('|')[1]
+    zvSta1 = zvSta1.upper()
+    zvAdr1 = a.split('|')[2]
+    zvAdr1Mtch = zvAdr1.split(' ')[0]
+    zvAdr1Mtch = zvAdr1Mtch + zvAdr1.split(' ')[1]
+    zvZip1 = a.split('|')[3]
+    zvLat1 = a.split('|')[4]
+    zvLon1 = a.split('|')[5]
 
     zvDriver.get("https://www.mapdevelopers.com/geocode_tool.php")
 
@@ -64,7 +68,24 @@ for a in zvAdrLst:
     zvZip = zvDriver.find_element_by_xpath(r'//*[@id="display_zip"]')
     zvZip = zvZip.text
     zvZip = zvZip.split('-')[0]
+    zvSta = zvDriver.find_element_by_xpath(r'//*[@id="display_city"]')
+    zvSta = zvSta.text
+    zvSta = str(zvSta)
+    zvSta = zvSta.upper()
+    zvAdr = zvDriver.find_element_by_xpath(r'//*[@id="display_address"]')
+    zvAdr = zvAdr.text
+    zvAdr = str(zvAdr)
+    zvAdr = zvAdr.upper()
+    zvAdrMtch = zvAdr.split(' ')[0]
+    zvAdrMtch = zvAdrMtch + zvAdr1.split(' ')[1]
 
-    print('{}|{}|{}|{}'.format(zvAdr1, zvLat, zvLon, zvZip))    
+    print('cty1: {}, cty2:{} | adr1:{}, adr2:{}'.format(zvSta1, zvSta, zvAdr1Mtch, zvAdrMtch))
 
-    zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip, zvLat, zvLon))
+    if zvAdr1Mtch == zvAdrMtch:
+        print('{}|{}|{}|{}'.format(zvAdr1, zvLat, zvLon, zvZip))
+        zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip, zvLat, zvLon))
+
+    else:
+        print('{}|{}|{}|{} ... orig'.format(zvAdr1, zvLat1, zvLon1, zvZip1))
+        zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip1, zvLat1, zvLon1))
+
