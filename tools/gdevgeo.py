870fcc5d95f97f22b3651d643bb25f6d5a0c14c8
Andy <andrew.mccrevan@eos-usa.com>
2022-02-25 15:52:52 -0500

    reg udt 2-25-22

--- tools/gdevgeo.py
+++ tools/gdevgeo.py
@@ -12,8 +12,8 @@ from myutils import zvFileUtil
 # started 4:10pm
 
 # --------------------------------------------- SETUP --------------------------------------------------------
-# -- options:
-#   --headless log-level=3
+# options...
+# --headless log-level=3
 chrome_options = Options()
 for a in sys.argv:
     chrome_options.add_argument(a)
@@ -22,7 +22,7 @@ for a in sys.argv:
 zvOs = os.name
 if zvOs == 'posix':
     chrome_options.binary_location = r"/usr/bin/google-chrome"
-    zvDrvPath = 'lnx/chromedriver'
+    zvDrvPath = '/home/CodeProjects/selenium-a-to-z/lnx/chromedriver'
 else:
     chrome_options.binary_location = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
     zvDrvPath = 'win/chromedriver.exe'
@@ -36,7 +36,7 @@ zvZip1 = ""
 zvLat1 = ""
 zvLon1 = ""
 
-zvAdrLst = zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr.txt", 'r')
+zvAdrLst = zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr.txt", 'r')
 zvAdrLst = zvAdrLst.splitlines()
 
 for a in zvAdrLst:    
@@ -47,45 +47,76 @@ for a in zvAdrLst:
     zvAdr1 = a.split('|')[2]
     zvAdr1Mtch = zvAdr1.split(' ')[0]
     zvAdr1Mtch = zvAdr1Mtch + zvAdr1.split(' ')[1]
+    zvAdr1Mtch = zvAdr1Mtch.upper()
     zvZip1 = a.split('|')[3]
     zvLat1 = a.split('|')[4]
     zvLon1 = a.split('|')[5]
 
-    zvDriver.get("https://www.mapdevelopers.com/geocode_tool.php")
+    zvDriver.get("https://www.bing.com/maps/")
 
     
-    zvAdrIn = zvDriver.find_element_by_xpath(r'//*[@id="address"]')
+    zvAdrIn = zvDriver.find_element_by_xpath(r'//*[@id="maps_sb"]')
     zvAdrIn.send_keys(zvAdr1)
-
-    zvDriver.find_element_by_xpath(r'//*[@id="search-form"]/div[1]/span[2]/button').click()
+    zvDriver.find_element_by_xpath(r'//*[@id="maps_sb_container"]/div[1]/div[2]/a').click()
 
     time.sleep(2)
+    
+    try:
+        #zvAdr = zvDriver.find_element_by_xpath(r'//*[@id="container"]/div[3]/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]')
+        zvAdrBlk = zvDriver.find_element_by_id("container")
+        zvAdrBlk = zvAdrBlk.text
+        zvAdrSpl = str(zvAdrBlk)
+        zvAdrSpl = zvAdrSpl.split("Bird's Eye")[1]
+        zvAdrSpl = zvAdrSpl.split("\n")[1]
+        zvAdrSpl = zvAdrSpl.split("\n")[0]
+        zvAdr = zvAdrSpl
+        zvAdrSpl = zvAdrSpl.split(' ')[0] + zvAdrSpl.split(' ')[1]
+        zvAdrSpl = zvAdrSpl.upper()
 
-    zvLat = zvDriver.find_element_by_xpath(r'//*[@id="display_lat"]')
-    zvLat = zvLat.text
-    zvLon = zvDriver.find_element_by_xpath(r'//*[@id="display_lng"]')
-    zvLon = zvLon.text
-    zvZip = zvDriver.find_element_by_xpath(r'//*[@id="display_zip"]')
-    zvZip = zvZip.text
-    zvZip = zvZip.split('-')[0]
-    zvSta = zvDriver.find_element_by_xpath(r'//*[@id="display_city"]')
-    zvSta = zvSta.text
-    zvSta = str(zvSta)
-    zvSta = zvSta.upper()
-    zvAdr = zvDriver.find_element_by_xpath(r'//*[@id="display_address"]')
-    zvAdr = zvAdr.text
-    zvAdr = str(zvAdr)
-    zvAdr = zvAdr.upper()
-    zvAdrMtch = zvAdr.split(' ')[0]
-    zvAdrMtch = zvAdrMtch + zvAdr1.split(' ')[1]
+        zvZip = zvAdr.rsplit(' ', 1)[1]    
 
-    print('cty1: {}, cty2:{} | adr1:{}, adr2:{}'.format(zvSta1, zvSta, zvAdr1Mtch, zvAdrMtch))
+        zvLatLon = zvAdrBlk.split('Search nearby')[1]    
+        zvLatLon = zvLatLon.split('\n')[2]
+        zvLat = zvLatLon.split(', ')[0]
+        zvLon = zvLatLon.split(', ')[1]
+        
+        if zvAdr1Mtch == zvAdrSpl:
+            print('{}|{}|{}|{}'.format(zvAdr1, zvLat, zvLon, zvZip))
+            zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip, zvLat, zvLon))
 
-    if zvAdr1Mtch == zvAdrMtch:
-        print('{}|{}|{}|{}'.format(zvAdr1, zvLat, zvLon, zvZip))
-        zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip, zvLat, zvLon))
+        else:
+            print('{}|{}|{}|{} ... orig'.format(zvAdr1, zvLat1, zvLon1, zvZip1))
+            zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}|orig\n'.format(zvAdId, zvAdr1, zvZip1, zvLat1, zvLon1))
 
-    else:
-        print('{}|{}|{}|{} ... orig'.format(zvAdr1, zvLat1, zvLon1, zvZip1))
-        zvFileUtil("/home/andy/CodeProjects/selenium-a-to-z/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip1, zvLat1, zvLon1))
+    except:
+        print('2nd attempt . . .')
+        try:
+            time.sleep(2)
+            #zvAdr = zvDriver.find_element_by_xpath(r'//*[@id="container"]/div[3]/div[1]/div[1]/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div/div[2]/div/div/div/div/div/div[2]/div/div[1]/div/div[2]')
+            zvAdrBlk = zvDriver.find_element_by_id("container")
+            zvAdrBlk = zvAdrBlk.text
+            zvAdrSpl = str(zvAdrBlk)
+            zvAdrSpl = zvAdrSpl.split("Bird's Eye")[1]
+            zvAdrSpl = zvAdrSpl.split("\n")[1]
+            zvAdrSpl = zvAdrSpl.split("\n")[0]
+            zvAdr = zvAdrSpl
+            zvAdrSpl = zvAdrSpl.split(' ')[0] + zvAdrSpl.split(' ')[1]
+            zvAdrSpl = zvAdrSpl.upper()
 
+            zvZip = zvAdr.rsplit(' ', 1)[1]    
+
+            zvLatLon = zvAdrBlk.split('Search nearby')[1]    
+            zvLatLon = zvLatLon.split('\n')[2]
+            zvLat = zvLatLon.split(', ')[0]
+            zvLon = zvLatLon.split(', ')[1]
+            
+            if zvAdr1Mtch == zvAdrSpl:
+                print('{}|{}|{}|{}'.format(zvAdr1, zvLat, zvLon, zvZip))
+                zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}\n'.format(zvAdId, zvAdr1, zvZip, zvLat, zvLon))
+
+            else:
+                print('{}|{}|{}|{} ... orig'.format(zvAdr1, zvLat1, zvLon1, zvZip1))
+                zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}|orig\n'.format(zvAdId, zvAdr1, zvZip1, zvLat1, zvLon1))
+        except:
+            print('{}|{}|{}|{} ... failed'.format(zvAdr1, zvLat1, zvLon1, zvZip1))
+            zvFileUtil("/home/CodeProjects/selenium-a-to-z/tools/geoadr_out.txt", 'a', '{}|{}|{}|{}|{}|err\n'.format(zvAdId, zvAdr1, zvZip1, zvLat1, zvLon1))
---
+++ tools/myutils.py
@@ -0,0 +1,14 @@
+# ------------------------- file utility ------------------------------------------
+# -- example: zvFileUtil("Help.txt", "r")
+def zvFileUtil(file, action, data=None):
+    _file = open(file, action)    
+    _wrt = data
+    if action == "w":
+        _file.write(_wrt)
+    if action == "a":
+        _file.write(_wrt)
+    if action == "r":
+        _wrt = _file.read()
+        return _wrt
+    _file.close()
+# ----------------------------------------------------------------------------------
